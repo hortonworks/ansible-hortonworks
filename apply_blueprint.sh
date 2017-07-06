c65d760fbce3a656dc0c78eaa17a0d1c773c324b
Sean Roberts <seanorama@users.noreply.github.com>
2017-07-06 15:11:07 +0100

    Simplify scripts (DRY: Don’t repeat yourself) - By sourcing the CLOUD_TO_USE check from a single file (set_cloud.sh)

--- apply_blueprint.sh
+++ apply_blueprint.sh
@@ -1,36 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ -z "$CLOUD_TO_USE" ]; then
-    echo "CLOUD_TO_USE environment variable must be set to one of the following: aws, azure, gce, openstack, static"
-    exit 1
-fi
+source "${BASH_SOURCE%/*}/set_cloud.sh"
 
-cloud_to_use=$(echo "$CLOUD_TO_USE" | tr '[:upper:]' '[:lower:]')
-case $cloud_to_use in
-aws|amazon)
-  cloud_to_use=aws
-  message="Cloud to be used is AWS.\nMake sure you've set the AWS authentication variables."
-  ;;
-azure|microsoft)
-  cloud_to_use=azure
-  message="Cloud to be used is Microsoft Azure.\nMake sure $HOME/.azure/credentials exists and it's correct."
-  ;;
-gce|google|gcp)
-  cloud_to_use=gce
-  message="Cloud to be used is Google Compute Engine.\nMake sure you've set the GCE authentication variables and JSON credentials file."
-  ;;
-openstack)
-  message="Cloud to be used is OpenStack.\nMake sure you've sourced the OpenStack RC file."
-  ;;
-static)
-  message="The static inventory will be used."
-  ;;
-*)
-  message="CLOUD_TO_USE environment variable was set to \"$CLOUD_TO_USE\" but must be set to one of the following: aws, azure, gce, openstack, static"
-  echo -e $message
-  exit 1
-  ;;
-esac
-
-echo -e $message
-ansible-playbook -i inventory/$cloud_to_use -e "cloud_name=$cloud_to_use" playbooks/apply_blueprint.yml
+ansible-playbook -i "inventory/${cloud_to_use}" -e "cloud_name=${cloud_to_use}" playbooks/apply_blueprint.yml
