cc50442187a61e128bacf893da3b39d377e5f10c
aLeX <agriffaut@gmail.com>
2019-02-07 22:19:21 +0100

    [FEATURE] Multi Cluster: allow to use multiple inventories

---
+++ set_inventory.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ "${cloud_to_use}" != "static" ]; then
+    inventory_to_use="${cloud_to_use}"
+    exit 0
+fi
+
+if [ -z "$INVENTORY_TO_USE" ]; then
+    echo -e "\e[94m[INFO]\e[0m - INVENTORY_TO_USE environment variable not set, defaulting to 'inventory/${cloud_to_use}'"
+    echo -e "See 'https://github.com/hortonworks/ansible-hortonworks/blob/master/INSTALL_${cloud_to_use}.md' for more details"
+    inventory_to_use="${cloud_to_use}"
+else
+    echo -e "\e[94m[INFO]\e[0m - INVENTORY_TO_USE environment variable set to '${INVENTORY_TO_USE}'"
+    inventory_to_use="${INVENTORY_TO_USE}"
+fi
+
+
