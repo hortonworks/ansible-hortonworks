471026abdb648344ef57f26d7abd2b9506a6cc8e
Alexandru Anghel <alexandru.anghel@gmail.com>
2019-06-03 16:10:37 +0100

    Fix for a bug causing the installation scripts to fail with cloud inventories.

--- set_inventory.sh
+++ set_inventory.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "${cloud_to_use}" != "static" ]; then
-    inventory_to_use="${cloud_to_use}"
-    exit 0
-fi
-
 if [ -z "$INVENTORY_TO_USE" ]; then
     echo -e "\e[94m[INFO]\e[0m - INVENTORY_TO_USE environment variable not set, defaulting to 'inventory/${cloud_to_use}'"
     echo -e "See 'https://github.com/hortonworks/ansible-hortonworks/blob/master/INSTALL_${cloud_to_use}.md' for more details"
